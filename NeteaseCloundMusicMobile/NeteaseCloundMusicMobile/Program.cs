3139af0840ee6f49f9f69976ba0759b6b18a987c
Desk_PC <T1246@DESKTOP-DKJ0EU9>
2021-05-16 20:58:16 +0800

    添加一个东西

--- NeteaseCloundMusicMobile/NeteaseCloundMusicMobile/Program.cs
+++ NeteaseCloundMusicMobile/NeteaseCloundMusicMobile/Program.cs
@@ -1,17 +1,13 @@
 using Blazored.LocalStorage;
 
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.AspNetCore.Components.WebAssembly.Http;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
+using NeteaseCloundMusicMobile.Client;
 using NeteaseCloundMusicMobile.Client.Services;
 
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace NeteaseCloundMusicMobile
